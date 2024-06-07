2d69cba76e4e7df8c5bd273140ff2675bfe67fb4
leonberkemeier <leonberkemeier@gmail.com>
2024-06-07 21:41:02 +0200

    thirdCommit

--- script.js
+++ script.js
@@ -22,14 +22,50 @@ var currentScrollPos = window.pageYOffset;
 
 
 
-// var prevScrollposs = window.pageYOffset;
-// window.onscroll = function() {
-// var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
+var typingEffect = new Typed (".multiText",{
+    strings:["Software","Help","Support","Kingz"],
+    loop: true,
+    typeSpeed: 200,
+    backSpeed: 300,
+    backDelay: 1500,
+});
 
-// }
+
+const card = document.getElementById("card");
+
+document.addEventListener("mousemove", (e) => {
+    rotateElement(e,card);
+});
+
+function rotateElement(event, element){
+    //where is teh mouse
+
+    const x = event.clientX;
+    const y = event.clientY;
+    // console.log(x,y)
+
+
+    //where the middle of card
+
+    //keep in MIND THIS IS ONLY FROM THE MIDDLE OF TEH SCREEN NOT THE CARD
+
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight /2;
+    // console.log(middleX)
+
+
+    // offset from mouse to middle of card
+
+    const offsetX = ((x - middleX)) ;
+    const offsetY = ((y - middleY));
+    // console.log(offsetX,offsetY)
+
+    const ratioOSX = ((x - middleX) / middleX)*15;
+    const ratioOSY = ((y - middleY) / middleY)*15;
+    // console.log(ratioOSX, ratioOSY);
+
+    //lets rotatae
+    element.style.setProperty("--rotateY", ratioOSX + "deg");
+    element.style.setProperty("--rotateX", -1 * ratioOSY + "deg");
+}
+// alert("hi")
