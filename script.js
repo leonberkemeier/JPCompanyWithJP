457a6c61c267c6a438bef6b099545c6e2331378b
leonberkemeier <leonberkemeier@gmail.com>
2024-06-10 21:47:41 +0200

    combine

--- script.js
+++ script.js
@@ -104,3 +104,25 @@ q1.addEventListener("click", () =>{
   
 });
 
+
+
+var coll = document.getElementsByClassName("collapsible");
+let list = document.querySelectorAll("collapsible")
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+
+        
+        this.classList.toggle("active");
+        
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+      
+    };
